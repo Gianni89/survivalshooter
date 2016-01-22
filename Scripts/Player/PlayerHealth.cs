7c609d1c510605f0e0f1d7d61f1f9ab64793bb2d
Gianni89 <tetsuo2019@gmail.com>
2016-01-22 17:49:08 +0000

    Replacing PlayerHealth.cs

--- Scripts/Player/PlayerHealth.cs
+++ Scripts/Player/PlayerHealth.cs
@@ -2,84 +2,107 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class PlayerHealth : MonoBehaviour
+namespace CompleteProject
 {
-    public int startingHealth = 100;
-    public int currentHealth;
-    public Slider healthSlider;
-    public Image damageImage;
-    public AudioClip deathClip;
-    public float flashSpeed = 5f;
-    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+	public class PlayerHealth : MonoBehaviour
+	{
+		public int startingHealth = 100;
+		public int currentHealth;
+		public Slider healthSlider;
+		public Image damageImage;
+		public AudioClip deathClip;
+		public float flashSpeed = 5f;
+		public Color flashColour = new Color (1f, 0f, 0f, 0.1f);
+		public ShieldBuff shieldBuff;
 
 
-    Animator anim;
-    AudioSource playerAudio;
-    PlayerMovement playerMovement;
-    PlayerShooting playerShooting;
-    bool isDead;
-    bool damaged;
+		Animator anim;
+		AudioSource playerAudio;
+		PlayerMovement playerMovement;
+		PlayerShooting playerShooting;
+		bool isDead;
+		bool damaged;
+		UpdateSlider updateSlider;
 
 
-    void Awake ()
-    {
-        anim = GetComponent <Animator> ();
-        playerAudio = GetComponent <AudioSource> ();
-        playerMovement = GetComponent <PlayerMovement> ();
-        playerShooting = GetComponentInChildren <PlayerShooting> ();
-        currentHealth = startingHealth;
-    }
+		void Awake ()
+		{
+			anim = GetComponent <Animator> ();
+			playerAudio = GetComponent <AudioSource> ();
+			playerMovement = GetComponent <PlayerMovement> ();
+			playerShooting = GetComponentInChildren <PlayerShooting> ();
+			shieldBuff = GetComponent<ShieldBuff> ();
+			updateSlider = healthSlider.GetComponent<UpdateSlider> ();
+
+			currentHealth = startingHealth;
+		}
 
 
-    void Update ()
-    {
-        if(damaged)
-        {
-            damageImage.color = flashColour;
-        }
-        else
-        {
-            damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
-        }
-        damaged = false;
-    }
+		void Update ()
+		{
+			if (damaged) 
+			{
+				damageImage.color = flashColour;
+			} 
+			else 
+			{
+				damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+			}
+
+			damaged = false;
+		}
 
 
-    public void TakeDamage (int amount)
-    {
-        damaged = true;
+		public void TakeDamage (int amount)
+		{
+			if (!shieldBuff.playerBuffed) 
+			{
+				damaged = true;
+				currentHealth -= amount;
+				updateSlider.SetSlider (currentHealth);
+				playerAudio.Play ();
 
-        currentHealth -= amount;
-
-        healthSlider.value = currentHealth;
-
-        playerAudio.Play ();
-
-        if(currentHealth <= 0 && !isDead)
-        {
-            Death ();
-        }
-    }
-
-
-    void Death ()
-    {
-        isDead = true;
-
-        playerShooting.DisableEffects ();
-
-        anim.SetTrigger ("Die");
-
-        playerAudio.clip = deathClip;
-        playerAudio.Play ();
-
-        playerMovement.enabled = false;
-        playerShooting.enabled = false;
-    }
+				if (currentHealth <= 0 && !isDead) 
+				{
+					Death ();
+				}
+			}
+		}
 
 
-    public void RestartLevel ()
-    {
-        Application.LoadLevel (Application.loadedLevel);
-    }
+		void Death ()
+		{
+			isDead = true;
+
+			playerShooting.DisableEffects ();
+
+			anim.SetTrigger ("Die");
+
+			playerAudio.clip = deathClip;
+			playerAudio.Play ();
+
+			playerMovement.enabled = false;
+			playerShooting.enabled = false;
+		}
+
+
+		public void RestartLevel ()
+		{
+			Application.LoadLevel (Application.loadedLevel);
+		}
+
+		public void HealPlayer (int amount)
+		{
+			if (currentHealth + amount > startingHealth) 
+			{
+				currentHealth = startingHealth;
+			} 
+			else 
+			{
+				currentHealth += amount;
+			}
+
+			updateSlider.SetSlider (currentHealth);
+		}
+	}
 }
